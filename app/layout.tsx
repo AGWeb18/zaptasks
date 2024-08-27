7eba5756c905a5adebb5548bacb27a3f6f03f73d
Anfro <aridding18@gmail.com>
2024-08-27 15:08:08 -0400

    updated with google tag&analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,6 +7,7 @@ import {
 } from "@clerk/nextjs";
 import "./globals.css";
 import { GoogleAnalytics } from "@next/third-parties/google";
+import Script from "next/script";
 
 export default function RootLayout({
   children,
@@ -16,6 +17,25 @@ export default function RootLayout({
   return (
     <ClerkProvider>
       <html lang="en">
+        <head>
+          <Script
+            src="https://www.googletagmanager.com/gtag/js?id=G-K9GLWHQEGZ"
+            strategy="afterInteractive"
+          />
+          <Script
+            src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID}`}
+            strategy="afterInteractive"
+          />
+          <Script id="google-analytics" strategy="afterInteractive">
+            {`
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+    gtag('config', '${process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID}');
+    gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ADS_ID}');
+  `}
+          </Script>
+        </head>
         <body>
           <main>{children}</main>
           <GoogleAnalytics gaId="G-K9GLWHQEGZ" />
