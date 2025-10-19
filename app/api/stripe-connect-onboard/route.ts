826c69fa60ef8b3cd400c723fe667380303ae9f9
Anfro <aridding18@gmail.com>
2025-10-19 14:44:01 -0600

    updated express settings

--- app/api/stripe-connect-onboard/route.ts
+++ app/api/stripe-connect-onboard/route.ts
@@ -71,10 +71,10 @@ export async function POST(req: NextRequest) {
         },
         controller: {
           fees: {
-            payer: "account",
+            payer: "application",
           },
           losses: {
-            payments: "stripe",
+            payments: "application",
           },
           stripe_dashboard: {
             type: "express",
